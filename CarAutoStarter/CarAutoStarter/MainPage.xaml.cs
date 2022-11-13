d6d8a80ff718c8738facb83dbe7b84571e52ed32
Matias Miller <matiasmiller@gmail.com>
2022-11-12 19:30:46 -0800

    Add project files.

---
+++ CarAutoStarter/CarAutoStarter/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CarAutoStarter
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNewCar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
